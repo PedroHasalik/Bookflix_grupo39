246796763c44ad8f84c10dec5f59db90705ea991
EnzoPujol <pujolenzo@gmail.com>
2020-05-06 19:56:43 -0300

    Arreglado error 404 al ejecutar run.py. Agregué un comentario de lo que falta solucionar en el mismo archivo.

--- run.py
+++ run.py
@@ -1,6 +1,37 @@
+import sys
 from Bookflix import create_app
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='Bookflix/templates')
 
-app = create_app()
+#Hay que encontrar la manera de importar las rutas  y los forms para no tener
+#el código de abajo en este archivo. Creo que se puede con la librería sys pero
+#no estoy seguro todavía. A solucionar.
+
+#                                               -Enzo
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title= 'Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check username and password and try again.', 'danger')
+    return render_template('login.html', title= 'Login', form=form)
 
 if __name__ == '__main__':
         app.run(debug=True)
