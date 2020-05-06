cdab1b2ec45838d928729dab3bcda0a084dc2e76
PedroHasalik <pedrohasa97@gmail.com>
2020-05-06 18:11:52 -0300

    Se arreglaros problemas de nombre de variables

--- Bookflix/__init__.py
+++ Bookflix/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from bookflix.config import Config
+from Bookflix.config import Config
 
 
 db = SQLAlchemy() #video 4
