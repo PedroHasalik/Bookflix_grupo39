c061eb8d84bc4117d2c29c89cf2ca777c5f7eb4c
PedroHasalik <pedrohasa97@gmail.com>
2020-05-20 17:47:40 -0300

    cambios en routes del usuario

--- Bookflix/users/routes.py
+++ Bookflix/users/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, url_for, flash, redirect, request, Blueprint
 from flask_login import login_user, current_user, logout_user, login_required
-from Bookflix.users.forms import (RegistrationForm, LoginForm)
-from Bookflix.models import User, Card, Profile
+from Bookflix.users.forms import (RegistrationForm, LoginForm, UpdateAccountForm)
+from Bookflix.models import User, Card
 from Bookflix import bcrypt, db
 
 users = Blueprint('users', __name__)
@@ -33,7 +33,7 @@ def login():
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
             #return redirect(next_page) if next_page else redirect(url_for('main.home'))
-            return redirect(url_for('users.profiles'))
+            return redirect(url_for('users.account'))
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
@@ -44,13 +44,33 @@ def logout():
     return redirect(url_for('main.home'))
 
 
+@users.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    card=Card.query.filter_by(owner=current_user).first()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        current_user.name = form.name.data
+        current_user.email = form.email.data
+        current_user.password= hashed_password
+        current_user.accountType= form.userType.data
+        card.number= form.number.data
+        card.security_number = form.securityNum.data
+        card.expiration_date = form.expDate.data 
+        db.session.commit()
+        flash('Los datos de la cuenta se guardaron!', 'success')
+        return redirect(url_for('users.account'))
+    elif request.method == 'GET':
+        form.name.data = current_user.name
+        form.email.data = current_user.email
+        form.password.data = current_user.password
+        form.number.data = card.number
+        form.userType.data = current_user.accountType
+        form.securityNum.data = card.security_number
+        form.expDate.data = card.expiration_date
+    return render_template('update_user.html', title='Account', form=form)
+
 @users.route("/debug")
 def debug():
     return render_template('debug.html', current_user=current_user)
-
-
-@users.route("/profiles")
-def profiles():
-    profiles = Profile.query.filter_by(owner=current_user).all()
-    cantProf = len(profiles)
-    return render_template('bookflix_profiles.html', current_user=current_user, profiles=profiles, cantProf=cantProf)
